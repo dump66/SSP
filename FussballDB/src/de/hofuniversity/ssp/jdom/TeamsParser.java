e14622e799f41a30bc6fafc604098a592b30cb00
student <student@192.168.219.128>
2017-04-05 10:36:21 +0200

    TeamsParser fertig

---
+++ FussballDB/src/de/hofuniversity/ssp/jdom/TeamsParser.java
@@ -0,0 +1,52 @@
+package de.hofuniversity.ssp.jdom;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import de.hofuniversity.ssp.teams.Team;
+
+public class TeamsParser {
+    
+    private Document doc;
+    private List<Team> teamList;
+    
+    public TeamsParser(String filename) throws JDOMException, IOException{
+	doc = new SAXBuilder().build(filename);
+	this.teamList = new LinkedList<Team>();
+    }
+
+    public void parse() throws JDOMException, IOException {
+	Element root = doc.getRootElement();
+	List<Element> teams = root.getChildren();
+	for (Element e : teams){
+	    Team currentTeam = new Team();
+	    currentTeam.setStadion(e.getChildText("stadion"));
+	    currentTeam.setTeamIconURL(e.getChildText("teamIconURL"));
+	    currentTeam.setTeamID(Integer.parseInt(e.getChildText("teamID")));
+	    currentTeam.setTeamName(e.getChildText("teamName"));
+	    for(Element player : e.getChildren("player")){
+		currentTeam.addPlayer(player.getText());
+	    }
+	    teamList.add(currentTeam);
+	}
+	
+    }
+    
+    public List<Team> getTeamList() {
+        return teamList;
+    }
+
+    public static void main(String[] args) throws JDOMException, IOException {
+	TeamsParser tParser = new TeamsParser("teams.xml");
+	tParser.parse();
+	for (Team t : tParser.getTeamList()){
+	    System.out.println(t.toString());
+	}
+    }
+}
--- FussballDB/src/de/hofuniversity/ssp/teams/Team.java
+++ FussballDB/src/de/hofuniversity/ssp/teams/Team.java
@@ -1,66 +1,71 @@
 package de.hofuniversity.ssp.teams;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Team {
 
-    private int teamID;
-    private String teamName;
-    private URL teamIconURL;
-    private String stadion;
+    private int		 teamID;
+    private String	 teamName;
+    private URL		 teamIconURL;
+    private String	 stadion;
     private List<String> players;
-    
-    public Team(){
-	
+
+    public Team() {
+
     }
 
     public int getTeamID() {
-        return teamID;
+	return teamID;
     }
 
     public void setTeamID(int teamID) {
-        this.teamID = teamID;
+	this.teamID = teamID;
     }
 
     public String getTeamName() {
-        return teamName;
+	return teamName;
     }
 
     public void setTeamName(String teamName) {
-        this.teamName = teamName;
+	this.teamName = teamName;
     }
 
     public URL getTeamIconURL() {
-        return teamIconURL;
+	return teamIconURL;
     }
 
-    public void setTeamIconURL(URL teamIconURL) {
-        this.teamIconURL = teamIconURL;
+    public void setTeamIconURL(String teamIconURL) {
+	try {
+	    this.teamIconURL = new URL(teamIconURL);
+	} catch (MalformedURLException e) {
+	    System.out.println("Warning: Could not create URL. Check XML File. URL-String:" + System.lineSeparator() + teamIconURL);
+	}
     }
 
     public String getStadion() {
-        return stadion;
+	return stadion;
     }
 
     public void setStadion(String stadion) {
-        this.stadion = stadion;
+	this.stadion = stadion;
     }
 
     public List<String> getPlayers() {
-	if (this.players == null){
+	if (this.players == null) {
 	    this.players = new LinkedList<String>();
 	}
-        return players;
+	return players;
     }
 
     public void addPlayer(String player) {
-        getPlayers().add(player);
+	getPlayers().add(player);
     }
-    
+
     @Override
     public String toString() {
-        return getTeamName() + "(" + getTeamID() + "): " + getStadion();
+	return getTeamName() + "(" + getTeamID() + "): " + getStadion();
     }
 }
